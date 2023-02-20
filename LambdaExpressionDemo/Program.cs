6a91cad8ae791774df0b18231a1a0f1fa5bcdfe3
Shailendra kumar pal <palshailendrakumar123@gmail.com>
2023-02-20 22:37:58 +0530

    lala la

---
+++ LambdaExpressionDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+
+namespace LambdaExpressionDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            List<Person> listPersonInCity = new List<Person>();
+            LambdaDemo.AddRecords(listPersonInCity);
+          //  LambdaDemo.Retrieving_TopTwoRecord_ForAgels_LessThanSixty(listPersonInCity);
+          //  LambdaDemo.CheckingForTeenagerPerson(listPersonInCity);
+          //  LambdaDemo.AllPersonsAverageAge(listPersonInCity);
+          //  LambdaDemo.CheckNameExistOrNot(listPersonInCity);
+            Console.ReadKey();
+           
+        }
+    }
+}
